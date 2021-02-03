4a1728bc56b19b1735569b527d54b8315752f652
HongXiangg <15042457680@163.com>
2021-02-03 14:42:14 +0800

    Add files via upload
    first

---
+++ Student.java
@@ -0,0 +1,80 @@
+package com.hongxiang.entities;
+
+public class Student {
+	private String studentId;
+	private String studentName;
+	private String gender;
+	private String classId;
+	private String teacherId;
+
+	/**
+	 * @return the studentId
+	 */
+	public String getStudentId() {
+		return studentId;
+	}
+
+	/**
+	 * @param studentId the studentId to set
+	 */
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	/**
+	 * @return the studentName
+	 */
+	public String getStudentName() {
+		return studentName;
+	}
+
+	/**
+	 * @param studentName the studentName to set
+	 */
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the classId
+	 */
+	public String getClassId() {
+		return classId;
+	}
+
+	/**
+	 * @param classId the classId to set
+	 */
+	public void setClassId(String classId) {
+		this.classId = classId;
+	}
+
+	/**
+	 * @return the teacherId
+	 */
+	public String getTeacherId() {
+		return teacherId;
+	}
+
+	/**
+	 * @param teacherId the teacherId to set
+	 */
+	public void setTeacherId(String teacherId) {
+		this.teacherId = teacherId;
+	}
+
+}
